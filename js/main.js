51a3a8b3636015f51a7b4c56eb7809e85fe32259
Ezonight <ablandon@heliacom.com>
2014-05-20 18:05:51 +0200

    Intégration; commit to pull

--- js/main.js
+++ js/main.js
@@ -13,35 +13,34 @@ WebFontConfig = {
 })();
 
 $(window).load(function(){
-if($(window).width()>767){
-    if($("#Content.home").length>0){
-        var height1=0;
-        var height2=0;
-        $("#Content.home>.row").each(function(){
-
-            $(this).find('section').each(function(){
-                height1 = $(this).outerHeight();
-                if(height1>=height2)height2=height1;
-                console.log(height2);
-            });
-            $(this).find('section').css('height',height2);
-            height1=0;
-            height2=0;
-            console.log("--");
-        });
-    }
-}
+    defineHeightSectionHome();
 });
 
+$(window).resize(function(){
+    defineHeightSectionHome();
+});
 $(document).ready(function(){
 
     $("#Agenda .slider").owlCarousel({
         items : 2, //2 items above 1000px browser width
-        itemsDesktop : [1000,2], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,2], // betweem 900px and 601px
-        itemsTablet: [600,2], //2 items between 600 and 0
-        itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+        itemsDesktop : [1200,2], //5 items between 1000px and 901px
+        itemsDesktopSmall : [992,4], // betweem 900px and 601px
+        itemsTablet: [768,3], //2 items between 600 and 0
+        itemsMobile : [480,2], // itemsMobile disabled - inherit from itemsTablet option
         pagination: false,
+        navigation:true
+
+    });
+
+    $("#Galerie .slider").owlCarousel({
+        items : 2, //2 items above 1000px browser width
+        itemsDesktop : [1200,2], //5 items between 1000px and 901px
+        itemsDesktopSmall : [992,4], // betweem 900px and 601px
+        itemsTablet: [768,3], //2 items between 600 and 0
+        itemsMobile : [480,2], // itemsMobile disabled - inherit from itemsTablet option
+        pagination: false,
+        itemsScaleUp:true,
+        autoPlay:true,
         navigation:true
 
     });
@@ -80,6 +79,61 @@ $(document).ready(function(){
     },1000);
 });
 
+//Definition d'une hauteur de la plus grande hauteur de la section de chaque ligne
+//sur la page d'accueil
+
+function defineHeightSectionHome(){
+    if($("#Content.home").length>0){
+        if($(window).width()>992){
+
+            var height1=0;
+            var height2=0;
+            $("#Content.home>.row").each(function(){
+
+                $(this).find('section').each(function(){
+                    height1 = $(this).outerHeight();
+                    if(height1>=height2)height2=height1;
+                    console.log(height2);
+                });
+                $(this).find('section').css('height',height2);
+                height1=0;
+                height2=0;
+                console.log("--");
+            });
+        }else{
+            $("#Content.home>.row").find('section').css('height','100%');
+        }
+    }
+}
+
+
+// --------------------------------------------------------------------------------------
+var up = "Welcome";
+var left = "Agenda";
+var right = "Contact";
+var down = "Galerie";
+var btnB = "Magazine";
+var btnA = "ImportantLinks";
+
+if ( window.addEventListener ) {
+    var myKey;
+    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+    $(window).click(function(e){
+        currentKey = e.target.id;
+        console.log(currentKey);
+        if(currentKey ==up) myKey = 38;
+        if(currentKey ==left) myKey = 37;
+        if(currentKey ==down) myKey = 40;
+        if(currentKey ==right) myKey = 39;
+        if(currentKey ==btnA) myKey = 65;
+        if(currentKey ==btnB) myKey = 66;
+        console.log(myKey);
+        kkeys.push( myKey );
+        if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+            alert('Konamicode!');
+        }
+    });
+}
 
 
 
